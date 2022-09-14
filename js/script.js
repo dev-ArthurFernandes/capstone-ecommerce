381d703f71235f7eb7a5ab90bc494ec7eea3762e
Arthur Fernandes <arthurdesousafernandes@gmail.com>
2022-09-14 00:02:49 -0300

    e-commerce phase 1.3

--- js/script.js
+++ js/script.js
@@ -50,20 +50,53 @@ function makeCards(item){
     butao.classList         = 'addCart'
     tagLi.id                = productId
     tagLi.classList         = 'produto'
+    tagDiv.appendChild(imagem)
 
     butao.addEventListener('click', () => {
-        let tagLi       = document.createElement('li')
-        let divImg      = document.createElement('div')
-        let imagem      = document.createElement('img')
-        let itemInfo    = document.createElement('div')
-        let tag         = document.createElement('span')
-        let spanName    = document.createElement('span')
-        let name        = document.createElement('h3')
+        
+        let tagUl               = document.querySelector('.carrinho')
+        
+        let tagLi               = document.createElement('li')
+        let divImg              = document.createElement('div')
+        let itemInfo            = document.createElement('div')
+        let tag                 = document.createElement('span')
+        let spanName            = document.createElement('span')
+        let spanPrice           = document.createElement('span')
+        let divButton           = document.createElement('div')
+        let bntMais             = document.createElement('button')
+        let bntMenos            = document.createElement('button')
+        let itemContador        = document.createElement('input')
+        let newImage            = document.createElement('img')
 
-        console.log(productId)
+        tagLi.id                = productId
+        divImg.className        = 'imgCart' 
+        itemInfo.className      = 'itemInfo'
+        divButton.className     = 'butoes'
+        bntMais.className       = 'but_+'
+        bntMenos.className      = 'but_-'
+        itemContador.className  = 'itemContador'
+        itemContador.value      = '0'
+        newImage.src            = productImg
+        newImage.alt            = productName
+
+
+        tag.innerHTML           = productTag
+        bntMais.innerHTML       = '+'
+        bntMenos.innerHTML      = '-'
+
+        spanName.appendChild(nome)
+        spanPrice.appendChild(preco)
+        divButton.append(bntMais,itemContador,bntMenos)
+        divImg.appendChild(newImage)
+        itemInfo.append(tag,spanName,spanPrice)
+        
+        tagLi.append(divImg,itemInfo,divButton)
+
+
+        tagUl.appendChild(tagLi)
     })
 
-    tagDiv.appendChild(imagem)
+    
     tagLi.append(tagDiv,categoria,nome,descrisao,preco,butao)
 
     return tagLi
